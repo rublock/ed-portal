5e3843945454889857dcab000632cdeb9fdcf6d1
mack <hypermail@yandex.ru>
2023-12-13 22:17:30 +0300

    Ajax отзывы о курсах

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
 
+from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
 
 
@@ -11,8 +14,6 @@ class MainPageView(TemplateView):
 
 
 class NewsListView(ListView):
-    template_name = "mainapp/news_list.html"
-
     model = mainapp_models.News
     paginate_by = 5
 
@@ -21,8 +22,6 @@ class NewsListView(ListView):
 
 
 class NewsCreateView(PermissionRequiredMixin, CreateView):
-    template_name = "mainapp/news_form.html"
-
     model = mainapp_models.News
     fields = "__all__"
     success_url = reverse_lazy("mainapp:news")
@@ -30,14 +29,10 @@ class NewsCreateView(PermissionRequiredMixin, CreateView):
 
 
 class NewsDetailView(DetailView):
-    template_name = "mainapp/news_detail.html"
-
     model = mainapp_models.News
 
 
 class NewsUpdateView(PermissionRequiredMixin, UpdateView):
-    template_name = "mainapp/news_form.html"
-
     model = mainapp_models.News
     fields = "__all__"
     success_url = reverse_lazy("mainapp:news")
@@ -45,8 +40,6 @@ class NewsUpdateView(PermissionRequiredMixin, UpdateView):
 
 
 class NewsDeleteView(PermissionRequiredMixin, DeleteView):
-    template_name = "mainapp/news_confirm_delete.html"
-
     model = mainapp_models.News
     success_url = reverse_lazy("mainapp:news")
     permission_required = ("mainapp.delete_news",)
@@ -69,7 +62,27 @@ class CoursesDetailView(TemplateView):
         context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
         context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
         context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
+        if not self.request.user.is_anonymous:
+            if not mainapp_models.CourseFeedback.objects.filter(
+                course=context["course_object"], user=self.request.user
+            ).count():
+                context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
+                    course=context["course_object"], user=self.request.user
+                )
+        context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(
+            course=context["course_object"]
+        ).order_by("-created", "-rating")[:5]
         return context
+
+
+class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
+    model = mainapp_models.CourseFeedback
+    form_class = mainapp_forms.CourseFeedbackForm
+
+    def form_valid(self, form):
+        self.object = form.save()
+        rendered_card = render_to_string("mainapp/includes/feedback_card.html", context={"item": self.object})
+        return JsonResponse({"card": rendered_card})
 
 
 class ContactsPageView(TemplateView):
