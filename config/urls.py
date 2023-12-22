8620f82e27911b018c438c6368b9a5426d454f37
mack <hypermail@yandex.ru>
2023-12-22 14:11:21 +0300

    hot fix

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(r'^i18n/', include('django.conf.urls.i18n')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path("", RedirectView.as_view(url="mainapp/")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
     path("authapp/", include("authapp.urls", namespace="authapp")),
