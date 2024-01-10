e5d11928633ca6b528ac40a39582155369ce9404
mack <hypermail@yandex.ru>
2024-01-10 16:27:15 +0300

    docker-compose admin

--- entrypoint_app.sh
+++ entrypoint_app.sh
@@ -2,4 +2,5 @@
 
 python app/manage.py migrate --noinput
 python app/manage.py loaddata 001_news 002_courses 003_lessons ./app/mainapp/fixtures/004_teachers.json
+python app/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'lpsys1@gmail.com', 'admin')"
 python app/manage.py runserver 0.0.0.0:8000
