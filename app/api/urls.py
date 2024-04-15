3be941f3987556ac6184d024faa5688541af813c
mack <hypermail@yandex.ru>
2024-04-15 18:13:53 +0300

    urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,9 +3,9 @@ from .views import NewsList, NewsDetail
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("", NewsList.as_view(), name="news_list"),
-    path("<int:pk>/", NewsDetail.as_view(), name="news_detail"),
+    path("", NewsList.as_view(), name="news_list_api"),
+    path("<int:pk>/", NewsDetail.as_view(), name="news_detail_api"),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui")
+    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
